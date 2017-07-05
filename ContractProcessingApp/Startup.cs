4bbc87fccb0d295d1560f102d0d524e0823cca09
Boakye Dankwa <bdankwa@iupui.edu>
2017-07-05 17:36:57 -0400

    Add project files.

---
+++ ContractProcessingApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContractProcessingApp.Startup))]
+namespace ContractProcessingApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
